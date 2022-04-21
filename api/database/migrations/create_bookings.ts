771d144d0a9c2f93f263ebc8335acce17cff58bc
Galitan-dev <galitan.dev@gmail.com>
2022-04-21 19:09:23 +0200

    fix: renamed bookings migration name (was Foodtrucks lol)

--- api/database/migrations/create_bookings.ts
+++ api/database/migrations/create_bookings.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Foodtrucks extends BaseSchema {
-  protected tableName = 'Foodtrucks'
+  protected tableName = 'Bookings'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
